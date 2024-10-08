7d63021cf5fa27192cde23441f1ee5febb6fb45c
Usman Saeed <uusaeed749@gmail.com>
2024-10-08 11:42:20 +0500

    video functionality added

---
+++ src/components/DemoVideo/DemoVideo.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./DemoVideo.css";
+
+const DemoVideo = () => {
+    // State to hold the current video source
+    const [videoSource, setVideoSource] = useState(require('../../assets/videos/litter_cam.mp4'));
+
+    // Handle video upload and change the video source
+    const handleVideoUpload = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const videoURL = URL.createObjectURL(file);
+            setVideoSource(videoURL); // Update the video source with the uploaded file
+        }
+    };
+
+    return (
+        <div className="video-container">
+            <h1 className="video-heading">Demo</h1>
+            {/* Video element */}
+            <video className="responsive-video" controls key={videoSource}>
+                <source src={videoSource} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
+            {/* Upload button */}
+            <input
+                type="file"
+                accept="video/*"
+                onChange={handleVideoUpload}
+                className="upload-button"
+            />
+        </div>
+    );
+};
+
+export default DemoVideo;
--- src/pages/Demo/Demo.js
+++ src/pages/Demo/Demo.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './Demo.css'; // You can style the page in this file
 import Header from '../../components/Header/Header';
 import ContactUs from '../../components/ContactUs/ContactUs';
+import DemoVideo from '../../components/DemoVideo/DemoVideo';
 
 const Demo = () => {
     return (
         <div className="demo-container">
             <Header />
+            <DemoVideo />
             <ContactUs />
         </div>
     );
